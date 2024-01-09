60efc0145b98546c63c7da044ec00ff677a88fe6
Kayleigh <kayleigh.mangion.h63613@mcast.edu.mt>
2024-01-09 15:15:37 +0000

    Added the template layout to the Main view

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -1,3 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
 <h1>All Cars Models</h1>
 <a href="{{route('cars.create')}}">Add a new Car</a>
 <a href="{{route('cars.show', 1)}}">Show a specific Car</a>
+@endsection
