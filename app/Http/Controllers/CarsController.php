b64414b896d3b67561c4222dc2478e1028b66683
KayleighMan <kaymangion@yahoo.com>
2024-01-09 22:30:18 +0000

    Populated Manufacturer dropdown list to obtain data from the Database and added partial view for dropdown menu

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Car;
+use App\Models\Manufacturer;
 
 class CarsController extends Controller
 {
     public function index()
     {
         $cars = Car::all();
-        return view('cars.index' , compact ('cars'));
+        $manufacturers = Manufacturer::orderBy('name')->pluck('name','id');
+        return view('cars.index' , compact ('cars', 'manufacturers'));
     }
 
     public function create()
